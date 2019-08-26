5e5211931d038fd4d8db6c5592cd134c292ecfb8
Mylestyberg <31495049+Mylestyberg@users.noreply.github.com>
2019-08-26 10:11:41 -0400

    ProjectCars first push

--- ProjectCars/get_screen.py
+++ ProjectCars/get_screen.py
@@ -2,19 +2,49 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
+from numpy import ones,vstack
+from numpy.linalg import lstsq
+from directkeys import PressKey, W, A, S, D
+from statistics import mean
 
 
-# just so this doesn't go on forever:
 
-def screen_record():
+
+def process_img(image):
+
+    original_image = image
+    gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    mask_white = cv2.inRange(gray_image, 200, 255)
+
+    mask_yw_image = cv2.bitwise_and(gray_image, mask_white)
+
+    processed_img = mask_yw_image
+
+    kernel_size = 5
+    gauss_gray = cv2.GaussianBlur(mask_yw_image, kernel_size)
+
+    low_threshold = 50
+    high_threshold = 150
+    canny_edges = cv2.Canny(gauss_gray, low_threshold, high_threshold)
+
+
+    return processed_img, original_image
+
+
+def main():
     last_time = time.time()
-    while (True):
-        # 800x600 windowed mode
-        printscreen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
-        print('loop took {} seconds'.format(time.time() - last_time))
+    while True:
+        screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
+        print('Frame took {} seconds'.format(time.time() - last_time))
         last_time = time.time()
-        cv2.imshow('window', cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        new_screen, original_image = process_img(screen)
+        cv2.imshow('window', new_screen)
+        cv2.imshow('window2', cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+        # cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
-screen_record()
+
+
+main()
